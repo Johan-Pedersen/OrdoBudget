e24ae53644e6a751108b4b304c0c364d6ce791e8
Johan Pedersen <sandbkpedersen.johan@gmail.com>
2024-07-05 13:40:32 +0200

    Initial controller and main window

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,106 @@
 package controller
+
+import (
+	excrptgrps "budgetAutomation/model/excrptGrps"
+	"budgetAutomation/model/util"
+	"context"
+	"log"
+	"strings"
+
+	req "budgetAutomation/model/requests"
+
+	"google.golang.org/api/sheets/v4"
+)
+
+func Submit(month int64, excrptPath string) {
+	var person int64 = 1
+
+	updateExcrptSheet(excrptPath)
+
+	budgetColA := getBudgetColA()
+	excrptgrps.InitExcrptGrps(budgetColA, month, person)
+
+	excrptsFromSheets := getExcrptsFromSheet()
+
+	accBalance := excrptgrps.LoadExcrptTotal(excrptsFromSheets, month)
+
+	updateReqs := updateBudgetReqs(budgetColA, accBalance, month, person)
+	batchUpdateReq := &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: updateReqs,
+	}
+	// Execute the BatchUpdate request
+	sheet := req.GetSheet()
+	ctx := context.Background()
+	_, updateBudgetErr := sheet.BatchUpdate(req.GetSpreadsheetId(), batchUpdateReq).Context(ctx).Do()
+
+	if updateBudgetErr != nil {
+		log.Fatalf("Unable to perform update operation: %v", updateBudgetErr)
+	}
+}
+
+/*
+Get all excrptGrps from the speadsheet
+*/
+func getBudgetColA() *sheets.ValueRange {
+	// Read all rows of col A in budget sheet.
+	budgetColARange := "A1:A"
+
+	sheet := req.GetSheet()
+	budgetColA, err := sheet.Values.Get(req.GetSpreadsheetId(), budgetColARange).Do()
+	if err != nil {
+		log.Fatalf("Unable to perform get: %v", err)
+	}
+	return budgetColA
+}
+
+/*
+Update the temporary sheet to hold excrpts
+*/
+func updateExcrptSheet(excrptPath string) {
+	sheet := req.GetSheet()
+	ctx := context.Background()
+
+	// Update excerpt sheet, before we begin
+	batchUpdateExcerptSheetReq := &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: excrptgrps.UpdateExcrptSheet(excrptPath),
+	}
+
+	_, excrptUpdateErr := sheet.BatchUpdate(req.GetSpreadsheetId(), batchUpdateExcerptSheetReq).Context(ctx).Do()
+
+	if excrptUpdateErr != nil {
+		log.Fatalf("Unable to perform update excerpt sheet operation: %v", excrptUpdateErr)
+	}
+}
+
+func getExcrptsFromSheet() *sheets.ValueRange {
+	sheet := req.GetSheet()
+	// Get Date, Amount and description
+	readRangeExrpt := "Udtrœk!A2:D"
+	excrpts, readExcrptsErr := sheet.Values.Get(req.GetSpreadsheetId(), readRangeExrpt).Do()
+	if readExcrptsErr != nil {
+		log.Fatalf("Unable to perform get: %v", readExcrptsErr)
+	}
+	return excrpts
+}
+
+func updateBudgetReqs(rows *sheets.ValueRange, accBalance float64, month, person int64) []*sheets.Request {
+	var updateReqs []*sheets.Request
+
+	for i, elm := range rows.Values {
+		if len(elm) != 0 {
+
+			total, notFoundErr := excrptgrps.GetTotal(elm[0].(string))
+
+			if notFoundErr == nil {
+				if total != 0.0 {
+					updateReqs = append(updateReqs, req.SingleUpdateReq(total, int64(i), util.MonthToColInd(month, person), 1685114351))
+				} else {
+					updateReqs = append(updateReqs, req.SingleUpdateReqBlank(int64(i), util.MonthToColInd(month, person), 1685114351))
+				}
+			} else if strings.EqualFold(strings.Trim(elm[0].(string), " "), "Faktisk balance") {
+				updateReqs = append(updateReqs, req.SingleUpdateReq(accBalance, int64(i), util.MonthToColInd(month, person), 1685114351))
+			}
+		}
+	}
+	return updateReqs
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"budgetAutomation/controller"
 	"log"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -10,22 +12,30 @@ import (
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Container")
-	myWindow.Resize(fyne.NewSize(300, 150))
-	moEntry := widget.NewEntry()
-	excrptEntry := widget.NewEntry()
+	initWindow := myApp.NewWindow("Container")
+	initWindow.Resize(fyne.NewSize(300, 150))
+	month := widget.NewEntry()
+	excrptPath := widget.NewEntry()
 
 	form := &widget.Form{
 		Items: []*widget.FormItem{ // we can specify items in the constructor
-			{Text: "Måned: ", Widget: moEntry},
-			{Text: "Bank udtræk: ", Widget: excrptEntry},
+			{Text: "Måned: ", Widget: month},
+			{Text: "Bank udtræk: ", Widget: excrptPath},
 		},
 		OnSubmit: func() { // optional, handle form submission
-			log.Println("Form submitted:", moEntry.Text)
-			myWindow.Close()
+			log.Println("Form submitted:", month.Text)
+			intMonth, err := strconv.ParseInt(month.Text, 10, 64)
+			if err != nil {
+				log.Fatal("Month not a number")
+			}
+
+			if intMonth >= 1 && intMonth <= 12 {
+				controller.Submit(intMonth, excrptPath.Text)
+			}
+			initWindow.Close()
 		},
 	}
 
-	myWindow.SetContent(form)
-	myWindow.ShowAndRun()
+	initWindow.SetContent(form)
+	initWindow.ShowAndRun()
 }
